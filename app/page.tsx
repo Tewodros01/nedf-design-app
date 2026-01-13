6365c55e55243b48e943ba2f9308035f3feec9fa
Tewodros01 <tewodrosm01@gmail.com>
2026-01-13 13:27:04 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,42 @@ import ProductGridList from "@/components/common/ProductGridList";
 import Brands from "@/components/homepage/Brands";
 import Header from "@/components/homepage/Header";
 import { Product } from "@/types/product.types";
+import { Review } from "@/types/review.types";
+
+export const reviewsData: Review[] = [
+  {
+    id: 1,
+    user: "Sarah M.",
+    content:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    rating: 5,
+    date: "August 14, 2023",
+  },
+  {
+    id: 2,
+    user: "Alex K.",
+    content:
+      "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.",
+    rating: 5,
+    date: "August 15, 2023",
+  },
+  {
+    id: 3,
+    user: "James L.",
+    content:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+    rating: 5,
+    date: "August 16, 2023",
+  },
+  {
+    id: 4,
+    user: "Mooen",
+    content:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+    rating: 5,
+    date: "August 17, 2023",
+  },
+];
 
 export const newArrivalsData: Product[] = [
   {
